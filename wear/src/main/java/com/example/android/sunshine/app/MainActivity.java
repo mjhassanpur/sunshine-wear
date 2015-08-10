e5f088fd89ddf9c2a7e677edd01ff80c842f79dd
mjhassanpur <mjhassanpur@gmail.com>
2015-08-09 22:52:01 -0700

    Set current time and date

--- wear/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ wear/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+    private TextView mTime;
+    private TextView mDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +22,26 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                mTime = (TextView) stub.findViewById(R.id.time);
+                mDate = (TextView) stub.findViewById(R.id.date);
+
+                mTime.setText(getCurrentTime());
+                mDate.setText(getCurrentDate());
             }
         });
+    }
+
+    private String getCurrentTime() {
+        Calendar cal = Calendar.getInstance();
+        DateFormat date = new SimpleDateFormat("HH:mm");
+        date.setTimeZone(cal.getTimeZone());
+        return date.format(cal.getTime());
+    }
+
+    private String getCurrentDate() {
+        Calendar cal = Calendar.getInstance();
+        DateFormat date = new SimpleDateFormat("EEE, MMM dd yyyy");
+        date.setTimeZone(cal.getTimeZone());
+        return date.format(cal.getTime()).toUpperCase();
     }
 }
